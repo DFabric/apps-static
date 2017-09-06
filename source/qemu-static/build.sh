fa84cf4063a1630456fd933722a811e45fb0be41
Julien REICHARDT <mi@jrei.ch>
2017-09-06 18:07:05 +0200

    Add qemu

---
+++ source/qemu-static/build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+wget -qO- https://download.qemu-project.org/qemu-$ver.tar.bz2 | tar xjf -
+
+cd qemu-$ver
+
+./configure --static --prefix=$DIR/$PACKAGE \
+  --extra-cflags=-DCONFIG_RTNETLINK \
+  --target-list=arm-linux-user,aarch64-linux-user
+
+# Apply patches from ALpine on the qemu source, mainly musl related
+# https://git.alpinelinux.org/cgit/aports/tree/main/qemu
+cd linux-user
+patch < $DIR/0006-linux-user-signal.c-define-__SIGRTMIN-MAX-for-non-GN.patch
+patch < $DIR/fix-sigevent-and-sigval_t.patch
+patch < $DIR/fix-sockios-header.patch
+patch < $DIR/musl-F_SHLCK-and-F_EXLCK.patch
+
+cd host/aarch64
+patch < $DIR/0001-linux-user-fix-build-with-musl-on-aarch64.patch
+
+cd ../../..
+make -j$nproc install
