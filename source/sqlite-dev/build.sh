b574ea15111fe0abca612b6505fd4a2aca4cf1f4
Julien Reichardt <mi@jrei.ch>
2017-09-15 22:26:55 +0200

    Use the builded zlib-dev

--- source/sqlite-dev/build.sh
+++ source/sqlite-dev/build.sh
@@ -14,11 +14,8 @@ cd sqlite-autoconf-$id
   CFLAGS="-DSQLITE_ENABLE_FTS3=1 -DSQLITE_ENABLE_COLUMN_METADATA=1 -DSQLITE_ENABLE_FTS3_PARENTHESIS -DSQLITE_SECURE_DELETE -DSQLITE_ENABLE_UNLOCK_NOTIFY -DSQLITE_ENABLE_RTREE=1 -DSQLITE_USE_URI -DSQLITE_ENABLE_DBSTAT_VTAB -DSQLITE_ENABLE_JSON1 -Iext/fts3"
 
 # Build & install to /usr/local
-make -j$nproc LDFLAGS=-static install
+make -j$nproc install-strip
 
 # Replace the prefix
 sed -i "s|prefix=$DIR/$PACKAGE|prefix=/usr|" $DIR/$PACKAGE/lib/pkgconfig/sqlite3.pc
 sed -i "s|libdir='$DIR/$PACKAGE/lib'|libdir='/usr/lib'|" $DIR/$PACKAGE/lib/libsqlite3.la
-
-# Strip
-strip $DIR/$PACKAGE/bin/sqlite3
--- source/zlib-dev/build.sh
+++ source/zlib-dev/build.sh
@@ -3,7 +3,7 @@
 wget -qO- https://downloads.sourceforge.net/project/libpng/zlib/$ver/zlib-$ver.tar.xz | tar xJf -
 cd zlib-$ver
 
-./configure --static --prefix=$DIR/$PACKAGE
+./configure --static --shared --prefix=$DIR/$PACKAGE
 
 make -j$nproc install
 
