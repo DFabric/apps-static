5ace3e8d270f355e28a0045418ff36fb94e9c96e
Julien Reichardt <mi@jrei.ch>
2017-11-26 13:35:58 +0100

    Add build flags for sqlite

--- source/sqlite-dev/build.sh
+++ source/sqlite-dev/build.sh
@@ -7,11 +7,25 @@ wget -qO- https://sqlite.org/$year/sqlite-autoconf-$id.tar.gz | tar zxf -
 
 cd sqlite-autoconf-$id
 
-./configure --prefix=$DIR/$PACKAGE \
+./configure \
+  --prefix=$DIR/$PACKAGE \
   --enable-static \
   --enable-shared \
+  --enable-threadsafe \
+  --enable-dynamic-extensions \
   LDFLAGS=-static \
-  CFLAGS="-DSQLITE_ENABLE_FTS3=1 -DSQLITE_ENABLE_COLUMN_METADATA=1 -DSQLITE_ENABLE_FTS3_PARENTHESIS -DSQLITE_SECURE_DELETE -DSQLITE_ENABLE_UNLOCK_NOTIFY -DSQLITE_ENABLE_RTREE=1 -DSQLITE_USE_URI -DSQLITE_ENABLE_DBSTAT_VTAB -DSQLITE_ENABLE_JSON1 -Iext/fts3"
+  CFLAGS="\
+  -DSQLITE_ENABLE_FTS3_PARENTHESIS \
+  -DSQLITE_ENABLE_FTS4 \
+  -DSQLITE_ENABLE_FTS5 \
+  -DSQLITE_ENABLE_COLUMN_METADATA \
+  -DSQLITE_SECURE_DELETE \
+  -DSQLITE_ENABLE_UNLOCK_NOTIFY \
+  -DSQLITE_ENABLE_RTREE \
+  -DSQLITE_USE_URI \
+  -DSQLITE_ENABLE_DBSTAT_VTAB \
+  -DSQLITE_MAX_VARIABLE_NUMBER=250000 \
+  -DSQLITE_ENABLE_JSON1 -Iext/fts5"
 
-# Build & install to /usr/local
+# Build & install
 make -j$nproc install-strip
