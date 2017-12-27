a7e2babfb9203d30558b3c37dc7d60be6f349efc
Julien Reichardt <mi@jrei.ch>
2017-12-27 17:10:03 +0100

    Add gc-dev

---
+++ source/gc-dev/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+wget -qO- https://github.com/ivmai/bdwgc/releases/download/v$ver/gc-$ver.tar.gz | tar xzf -
+cd gc-$ver
+
+./configure \
+	CFLAGS="-D_GNU_SOURCE -DNO_GETCONTEXT -DUSE_MMAP -DHAVE_DL_ITERATE_PHDR -DIGNORE_DYNAMIC_LOADING" \
+	--enable-cplusplus \
+	--disable-parallel-mark \
+	--enable-munmap
+
+make -j$nproc DESTDIR="$DIR/$PACKAGE" install-strip
