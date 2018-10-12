62fbfff88b0cec8544def6752f017098beb6a4ae
Julien Reichardt <mi@jrei.ch>
2018-10-12 17:41:41 +0200

    Add libunwind-dev to fix static linking

--- source/dppm-static/build.sh
+++ source/dppm-static/build.sh
@@ -9,6 +9,6 @@ cd DFabric-dppm-*
 # Install libraries
 shards install
 
-crystal build --progress --threads $nproc --release --static --no-debug src/dppm.cr -o ../$PACKAGE/bin/dppm
+crystal build --progress --threads $nproc --release --static --link-flags -lunwind --no-debug src/dppm.cr -o ../$PACKAGE/bin/dppm
 strip ../$PACKAGE/bin/dppm
 
