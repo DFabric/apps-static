c38bda116101d57a1171ec93de55b36850b1899b
Julien Reichardt <mi@jrei.ch>
2018-10-26 16:38:12 +0200

    Fix static linking

--- source/dppm-static/build.sh
+++ source/dppm-static/build.sh
@@ -8,6 +8,12 @@ cd dppm
 # Install libraries
 shards install
 
-crystal build --progress --threads $nproc --release --static --link-flags -lunwind --no-debug src/dppm.cr -o ../$PACKAGE/bin/dppm
+# Static build fix
+cat >> src/dppm.cr <<EOF
+require "llvm/lib_llvm"
+require "llvm/enums"
+EOF
+
+crystal build --progress --threads $nproc --release --static --no-debug src/dppm.cr -o ../$PACKAGE/bin/dppm
 strip ../$PACKAGE/bin/dppm
 
