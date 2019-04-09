1ddbb2056a36a00c2094d945e72d1b9b4cbda3c8
Julien Reichardt <mi@jrei.ch>
2019-04-09 18:35:35 +0200

    Add Minetest

---
+++ source/minetest-static/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# https://dev.minetest.net/Compiling_Minetest
+wget -qO- https://github.com/minetest/minetest/archive/$ver.tar.gz | tar xzf -
+
+cd minetest-$ver/games
+wget -qO- https://github.com/minetest/minetest_game/archive/$ver.tar.gz | tar xzf -
+mv minetest_game-$ver minetest_game
+cd ..
+
+
+# Build
+cmake -DENABLE_CURL=0 \
+      -DENABLE_GETTEXT=0 \
+      -DBUILD_CLIENT=0 \
+      -DBUILD_SERVER=1 \
+      -DENABLE_SYSTEM_JSONCPP=1 \
+      -DENABLE_LUAJIT=1 \
+      -DCMAKE_EXE_LINKER_FLAGS=-static
+
+make -j$(nproc) DESTDIR="$DIR/$PACKAGE"
+mv bin $DIR/$PACKAGE
