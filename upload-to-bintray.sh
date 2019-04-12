e0c18c2bb4ce31a066e46f3363f20881b9bcd12c
Julien Reichardt <mi@jrei.ch>
2019-04-12 13:45:08 +0200

    Change echo to printf
    printf is more portable than echo, speicially when using \n

--- upload-to-bintray.sh
+++ upload-to-bintray.sh
@@ -8,12 +8,12 @@ BINTRAY_REPO=builds/latest/
 
 for package_path in build/*; do
   package_name=${package_path##*/}
-  echo "delete $package_name"
+  printf "delete $package_name\n"
   curl -u$BINTRAY_USER_API_KEY -X DELETE $BINTRAY_ORG/$package_name
-  echo "\npost $package_name"
+  printf "\npost $package_name\n"
   curl -u$BINTRAY_USER_API_KEY -T $package_path $BINTRAY_ORG/$BINTRAY_REPO/$package_name
-  echo "\n"
+  printf "\n\n"
 done
 
-echo "publish to $BINTRAY_ORG/$BINTRAY_REPO"
+printf "publish to $BINTRAY_ORG/$BINTRAY_REPO\n"
 curl -u$BINTRAY_USER_API_KEY -X POST $BINTRAY_ORG/$BINTRAY_REPO/publish
