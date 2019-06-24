af4d5e91b1a9f00bace13f6ad1c2084067b7596a
Julien Reichardt <mi@jrei.ch>
2019-06-24 13:18:06 +0200

    Fix mattermost build

--- source/mattermost-static/build.sh
+++ source/mattermost-static/build.sh
@@ -1,27 +1,28 @@
 #!/bin/sh
 
 export GOPATH=/tmp
-mkdir -p $GOPATH/src/github.com/mattermost
-cd $GOPATH/src/github.com/mattermost
+mattermost_path=$GOPATH/src/github.com/mattermost
+
+mkdir -p $mattermost_path
+cd $mattermost_path
 
 wget -qO- https://github.com/mattermost/mattermost-webapp/archive/v$ver.tar.gz | tar xzf -
 mv mattermost-webapp* mattermost-webapp
 cd mattermost-webapp
 npm install
 cd node_modules/mattermost-redux
-npm install
+npm install || true
 npm run build
 cd ../..
 npm run build
 
-cd ..
-go get -u github.com/golang/dep/cmd/dep
+cd $mattermost_path
 wget -qO- https://github.com/mattermost/mattermost-server/archive/v$ver.tar.gz | tar xzf -
 mv mattermost-server* mattermost-server
 
-cd mattermost-server
-$GOPATH/bin/dep ensure
+export GO111MODULE=on
 export LDFLAGS='-extldflags -static'
+cd mattermost-server
 make build
 cp config/default.json config/config.json
 make package
