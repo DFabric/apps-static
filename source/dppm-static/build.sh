122c5348060c7d4cafd00a3df3af5258b5b2556a
Julien Reichardt <mi@jrei.ch>
2019-08-24 20:17:47 +0200

    Fix DPPM for Alpine Edge

--- source/dppm-static/build.sh
+++ source/dppm-static/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo '@v3.9 http://dl-cdn.alpinelinux.org/alpine/v3.9/community' >>/etc/apk/repositories
+apk add --update shards@v3.9
+
 # Prepare directories
 git clone https://github.com/DFabric/dppm
 mkdir $PACKAGE/bin
