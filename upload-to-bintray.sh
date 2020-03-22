b5e5aa77366bd1550bebab725f275fe60a9b2793
Julien Reichardt <git@jrei.ch>
2020-03-22 21:54:03 +0100

    Improve upload script variables

--- upload-to-bintray.sh
+++ upload-to-bintray.sh
@@ -3,17 +3,21 @@
 # Script to upload artifacts to Bintray
 
 BINTRAY_USER_API_KEY=$1
-BINTRAY_ORG=https://api.bintray.com/content/dfabric/apps-static
-BINTRAY_REPO=builds/latest/
+BINTRAY_ORG=dfabric/apps-static
+BINTRAY_PACKAGE=builds
+BINTRAY_VERSION=latest
+
+BINTRAY_CONTENT_API=https://api.bintray.com/content/$BINTRAY_ORG
+BINTRAY_CONTENT_API_PACKAGE=$BINTRAY_CONTENT_API/$BINTRAY_PACKAGE/$BINTRAY_VERSION
 
 for package_path in build/*; do
   package_name=${package_path##*/}
   printf "delete $package_name\n"
-  curl -u$BINTRAY_USER_API_KEY -X DELETE $BINTRAY_ORG/$package_name
+  curl -u$BINTRAY_USER_API_KEY -X DELETE $BINTRAY_CONTENT_API/$package_name
   printf "\npost $package_name\n"
-  curl -u$BINTRAY_USER_API_KEY -T $package_path $BINTRAY_ORG/$BINTRAY_REPO/$package_name
+  curl -u$BINTRAY_USER_API_KEY -T $package_path $BINTRAY_CONTENT_API_PACKAGE/$package_name
   printf "\n\n"
 done
 
-printf "publish to $BINTRAY_ORG/$BINTRAY_REPO\n"
-curl -u$BINTRAY_USER_API_KEY -X POST $BINTRAY_ORG/$BINTRAY_REPO/publish
+printf "publish to $BINTRAY_CONTENT_API_PACKAGE\n"
+curl -u$BINTRAY_USER_API_KEY -X POST $BINTRAY_CONTENT_API_PACKAGE/publish
